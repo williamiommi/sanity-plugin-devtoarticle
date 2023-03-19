975f144fd0b79b09de36feabe2112302475a627d
williamiommi <william.iommi@gmail.com>
2023-03-19 14:17:19 +0100

    update configuration with tailwind and css export

--- package.config.ts
+++ package.config.ts
@@ -1,10 +1,29 @@
 import {defineConfig} from '@sanity/pkg-utils'
+import copy from 'rollup-plugin-copy'
+import postcss from 'rollup-plugin-postcss'
+import path from 'path'
 
 export default defineConfig({
   legacyExports: true,
   dist: 'dist',
   tsconfig: 'tsconfig.dist.json',
-
+  rollup: {
+    plugins: [
+      postcss({
+        extract: path.resolve('./dist/bundle.min.css'),
+        minimize: true,
+        plugins: [require('tailwindcss'), require('autoprefixer')],
+      }),
+      copy({
+        targets: [
+          {
+            src: './node_modules/katex/dist/fonts/**/*',
+            dest: path.resolve('./dist/fonts'),
+          },
+        ],
+      }),
+    ],
+  },
   // Remove this block to enable strict export validation
   extract: {
     rules: {
